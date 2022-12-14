78ad43fd9339b64f9e92290a713198de7a3556d1
Dell <Dell@DESKTOP-3M30ENU>
2022-12-14 11:32:15 +0530

    First commit....

---
+++ GitDemo/src/Demo/Test1.java
@@ -0,0 +1,24 @@
+package Demo;
+
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+ 
+public class Test1 {
+	@Test
+		   public void demo() {
+       String driverExecutablepath = "C:\\Navya\\Selenium\\Drivers\\chromedriver.exe";
+        System.setProperty("Webdriver.edge.driver", driverExecutablepath);
+       WebDriver driver = new ChromeDriver();
+      driver.manage().window().maximize();
+      driver.get("https://opensource-demo.orangehrmlive.com/");
+      Assert.assertTrue(driver.getCurrentUrl().contains("opensource-demo"),"URL Match");
+}
+
+ 
+
+}
